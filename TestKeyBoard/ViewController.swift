c2651ab56e28037993ba527c2366df78e2d73126
Edwin <epolo@wawand.co>
2016-09-12 16:48:30 -0500

    Initial Commit

---
+++ TestKeyBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestKeyBoard
+//
+//  Created by Edwin Polo on 9/12/16.
+//  Copyright © 2016 Edwin Polo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
